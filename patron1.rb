ccb4dd7a60d104ee7ec17924c0329373b99b5383
Fabian Conejo <fabianconejosegu@gmail.com>
2021-02-01 16:13:31 -0600

    loops and patterns exercises on ruby. Project by desafio latam.

---
+++ patron1.rb
@@ -0,0 +1,7 @@
+number = ARGV[0].to_i
+
+for i in (1..number) 
+
+    i.odd? ? print('*') : print('.')
+
+end
---
+++ patron2.rb
@@ -0,0 +1,5 @@
+number = ARGV[0].to_i
+
+for i in (0...number)
+    (i % 4 == 0 or i % 4 == 1) ? print('*') : print('.') 
+end 
